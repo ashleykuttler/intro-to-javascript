d36aaee1dbcd389de667e56a77521411ceed0ca0
Ashley Kuttler <ashleykuttler@gmail.com>
2019-04-27 22:49:44 -0500

    added filter click function with if statments...still not retrieving dates

--- static/js/app.js
+++ static/js/app.js
@@ -1,13 +1,14 @@
 // from data.js
-var tableData = data;
+const tableData = data;
 
-// YOUR CODE HERE!
-var filterTable = d3.select("#filter-btn");
-var inputDate = d3.select("#datetime");
-var tbody = d3.select("tbody");
-var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
-
-var getData = (userInput) => {
+// create variables 
+const filterTable = d3.select("#filter-btn");
+const reset_table = d3.select("#reset-btn");
+const inputDate = d3.select("#datetime");
+const tbody = d3.select("tbody");
+const columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+// function to retrieve data and append to table form in tbody of html 
+const getData = (userInput) => {
     userInput.forEach(ufo_sightings => {
         let row = tbody.append("tr")
         columns.forEach(column => row.append("td").text(ufo_sightings[column])
@@ -17,6 +18,29 @@ var getData = (userInput) => {
 
 getData(tableData)
 
-// filterTable.on("click", function(){
-
-// })
+filterTable.on("click", function(){
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    //assign value of inputdate to userDate variable
+    const userDate = inputDate.property("value").trim();
+    //filter datatable for dates equal to user input
+    const filterDate = tableData.filter(tableData => tableData.datetime === userDate);
+    console.log(filterDate)
+    tbody.html("");
+    //add filtered data to table
+    let response = {
+        filterDate
+    }
+    //if response length is not equal to zero run getData function to retrieve results
+    if (response.filterDate.length !== 0) {
+        getData(filterDate);
+    }   
+    //Otherwise return error message
+        else {
+            tbody.append("tr").append("td").text("No Results");
+        }
+    })
+    reset_table.on("click", () => {
+        tbody.html("");
+        getData(tableData)
+      });
