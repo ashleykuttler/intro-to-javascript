0e3918018173caf300e7a9ed44f1da022b39a93f
Ashley Kuttler <ashleykuttler@gmail.com>
2019-04-27 21:57:00 -0500

    added variables and function to retrieve data

--- static/js/app.js
+++ static/js/app.js
@@ -2,10 +2,21 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-const filterTable = d3.select("#filter-btn");
+var filterTable = d3.select("#filter-btn");
+var inputDate = d3.select("#datetime");
+var tbody = d3.select("tbody");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
-filterTable.on("click", function(){
-    
+var getData = (userInput) => {
+    userInput.forEach(ufo_sightings => {
+        let row = tbody.append("tr")
+        columns.forEach(column => row.append("td").text(ufo_sightings[column])
+        )
+    })
+}
 
+getData(tableData)
 
-})
+// filterTable.on("click", function(){
+
+// })
